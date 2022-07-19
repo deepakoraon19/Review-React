8f8a4f134785e8375b647244167c1ea75dbb4c57
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-07-19 13:34:22 +0530

    Updates

---
+++ precache-manifest.ce5638bfbe66f97d98a26974124d4a84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326084a705ccf63b46ead4a75e70c3e4",
+    "url": "/Review-React/index.html"
+  },
+  {
+    "revision": "d5727e168f819f5cbe9c",
+    "url": "/Review-React/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/Review-React/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Review-React/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5727e168f819f5cbe9c",
+    "url": "/Review-React/static/js/main.753fbb4e.chunk.js"
+  },
+  {
+    "revision": "49daa9e98247e96fb584",
+    "url": "/Review-React/static/js/runtime-main.a49dd512.js"
+  }
+]);
